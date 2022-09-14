837a9be15771eca2fe99841e31cea2b36c34afbe
chikqje <jessica.aulia11@ui.ac.id>
2022-09-14 12:34:08 +0700

    update

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    list_item = CatalogItem.objects.all()
+    context = {
+    'list_item': list_item,
+    'nama': 'Jessica Aulia',
+    'NPM': '2106633512'
+}
+    return render(request, "katalog.html",context)
